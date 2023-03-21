67c3a997ebb71a7bde56932b119d06f94af442b5
Billy Zovan Ginting <billyzovanginting@gmail.com>
2023-03-21 19:18:25 +0700

    challanges 4

--- member/member_impl.go
+++ member/member_impl.go
@@ -9,29 +9,29 @@ type MemberImpl struct {
 	Reasson   string
 }
 
-func (m *MemberImpl) GetMember(id int) (MemberImpl, error) {
-	data := map[int]MemberImpl{
-		1: MemberImpl{
-			Nama:      "Billy",
-			Alamat:    "jln jamin ginting, berastagi",
-			Pekerjaan: "Pelajar",
-			Reasson:   "Karena Peformanya yang unggul serta mudah digunakan",
-		},
-		2: MemberImpl{
-			Nama:      "Bima",
-			Alamat:    "jln jamin ginting, pajak roga",
-			Pekerjaan: "Pelajar",
-			Reasson:   "Karena lagi booming dan banyak di butuhkan",
-		},
-		3: MemberImpl{
-			Nama:      "Rima",
-			Alamat:    "jln jamin ginting, kabanjahe",
-			Pekerjaan: "Pelajar",
-			Reasson:   "karena populer",
-		},
-	}
+var Data = map[int]MemberImpl{
+	1: {
+		Nama:      "Billy",
+		Alamat:    "jln jamin ginting, berastagi",
+		Pekerjaan: "Pelajar",
+		Reasson:   "Karena Peformanya yang unggul serta mudah digunakan",
+	},
+	2: {
+		Nama:      "Bima",
+		Alamat:    "jln jamin ginting, pajak roga",
+		Pekerjaan: "Pelajar",
+		Reasson:   "Karena lagi booming dan banyak di butuhkan",
+	},
+	3: {
+		Nama:      "Rima",
+		Alamat:    "jln jamin ginting, kabanjahe",
+		Pekerjaan: "Pelajar",
+		Reasson:   "karena populer",
+	},
+}
 
-	if res, ok := data[id]; ok {
+func (m *MemberImpl) GetMember(id int) (MemberImpl, error) {
+	if res, ok := Data[id]; ok {
 		return res, nil
 
 	}
